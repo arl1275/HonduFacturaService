7abbebb535665a0e093c5020eca188c926369c29
arnulfo1275 <arnulfo1275@gmail.com>
2025-08-20 22:11:10 -0600

    invoice show view

--- app/modules/invoice/pages/InvoiceShow.tsx
+++ app/modules/invoice/pages/InvoiceShow.tsx
@@ -1,40 +1,80 @@
 import { StackScreenProps } from "@react-navigation/stack";
 import React, { useEffect, useState } from "react";
-import { TouchableOpacity, View, Text, Button } from "react-native";
+import { TouchableOpacity, View, Text, Button, FlatList } from "react-native";
 import { RootStackParamList } from "../indexInvoice";
-import { invoice } from "@/storage/invoice";
+import { invoice, invoicesconfig } from "@/storage/invoice";
 import styles from "@/assets/styles/styles";
 import Ionicons from "react-native-vector-icons/Ionicons";
 import { formated_invoice_number } from "../utils/InvoiceNumberGenerator";
-
+import { getCompany_by_ID } from "@/storage/company.storage";
+import { getInvoicesconfig_by_id } from "@/storage/invoiceconfig.storage";
+import { company } from "@/storage/empresa";
 type props = StackScreenProps<RootStackParamList, "InvoiceShow">
 
 
-const InvoiceShowPage = ({ route, navigation } : props) =>{
+const InvoiceShowPage = ({ route, navigation }: props) => {
     const { item } = route.params;
     const [_invoice_, setInvoice] = useState<invoice>();
+    const [comp, setComp] = useState<company>()
+    const [invoc, setInvoiceConfig] = useState<invoicesconfig>()
 
-    useEffect(()=>{
+    useEffect(() => {
         setInvoice(item);
+        
     }, [item]);
+
+    const GetCompany = (value: number) => {
+        let val = getCompany_by_ID(value);
+        setComp(val);
+    }
+
+    const GetInvoiceConfig = (value : number) =>{
+        let val = getInvoicesconfig_by_id(value);
+        setInvoiceConfig(val[0]);
+    }
 
     const oncancel = () => { setInvoice(undefined), navigation.navigate("HomeInvoice") };
 
-    return(
-        <View style={[{flex : 1}]}>
+    return (
+        <View style={[{ flex: 1 }]}>
             <View style={[styles.flexcomponentsRow, { margin: 5 }]}>
                 <TouchableOpacity onPress={() => oncancel()}>
                     <Ionicons name="chevron-back" size={30} color="black" />
                 </TouchableOpacity>
-                <Text style={[styles.paragraph, styles.textalingleft, { color: 'black' }]}>Invoice Number ( {_invoice_? formated_invoice_number(_invoice_.formato_general.numero_de_factura) : 'N/A'} )</Text>
+                <Text style={[styles.paragraph, styles.textalingleft, { color: 'black' }]}>Invoice Number ( {_invoice_ ? formated_invoice_number(_invoice_.formato_general.numero_de_factura) : 'N/A'} )</Text>
+            </View>
+
+            <View style={[styles.flexcomponentsRow, {}]}>
+                <Button title="Print" color={'black'} />
+                <Button title="Share" color={'black'} />
             </View>
 
             <View>
-                <Button title="Print" color={'black'}/>
-                <Button title="Share" color={'black'}/>
-            </View>
-            
-            <View>
+                {
+                    !_invoice_ ? null :
+                        <View style={[{ borderWidth: 1, borderColor: 'grey', borderRadius: 7 }]}>
+                            <Text>{comp ? comp.companyname : 'Waiting response'}</Text>
+                            <Text>{_invoice_.formato_general.encabezado}</Text>
+
+                            <Text>{comp ? comp.direccion_company  : null}</Text>
+                            <Text>{comp ? comp.numero_telefono_compay  : null}</Text>
+                            <Text>{comp ? comp.rtn  : null }</Text>
+                            <Text>{ }</Text>
+
+
+                            <Text>{formated_invoice_number(_invoice_.formato_general.numero_de_factura)}</Text>
+                            <Text>{_invoice_.formato_general.comprador}</Text>
+                            <Text>{_invoice_.formato_general.comprador_rtn}</Text>
+                            <Text>{ }</Text>
+                            <Text>{ }</Text>
+                            <Text>{ }</Text>
+                            <Text>{ }</Text>
+                            <Text>{ }</Text>
+                            <Text>{ }</Text>
+                            <Text>{ }</Text>
+                            <Text>{ }</Text>
+                        </View>
+                }
 
             </View>
 
