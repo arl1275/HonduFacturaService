0b33957c804bd266352dc3733980644662fdf540
arnulfo1275 <arnulfo1275@gmail.com>
2025-10-14 22:40:41 -0600

    creating inserting lot modals and storage

---
+++ storage/insertlot.storage.ts
@@ -0,0 +1,41 @@
+import { MMKV } from 'react-native-mmkv';
+import { InsertLot } from './modals/insertlot_modal';
+
+const storage = new MMKV();
+const STORAGE_KEY = 'InsertLots';
+
+export const getInsertLots = (): InsertLot[] => {
+  const json = storage.getString(STORAGE_KEY);
+  return json ? JSON.parse(json) : [];
+};
+
+export const saveLotsInserted = (companies: InsertLot[]) => {
+  storage.set(STORAGE_KEY, JSON.stringify(companies));
+};
+
+export const addinsertlot = (insertlot: InsertLot) => {
+  const companies = getInsertLots();
+  companies.push(insertlot);
+  saveLotsInserted(companies);
+};
+
+export const updateinsertlot = (updated: InsertLot) => {
+  const companies = getInsertLots().map(c =>
+    c.id === updated.id ? updated : c
+  );
+  saveLotsInserted(companies);
+};
+
+export const deleteinsertlot = (id: number) => {
+  const companies = getInsertLots().filter(c => c.id !== id);
+  saveLotsInserted(companies);
+};
+
+export const clearLotsInserted = () => {
+  storage.delete(STORAGE_KEY);
+};
+
+export const getInsertLots_by_idWH = (id : number) =>{
+   const companies = getInsertLots().filter(c => c.inventory_destiny_id === id);
+   return companies;
+}
---
+++ storage/modals/insertlot_modal.ts
@@ -0,0 +1,21 @@
+export interface supplier{
+    id: number, 
+    name : string, 
+    code : string,
+    active : boolean
+}
+
+export interface InsertLot {
+  id: number;
+  inventory_destiny_id : number,
+  insert_lot_num: string; // sequence: [Inventory - YYYY.MM.DD - xxxx]
+  inserter: string;
+  id_supplier: number;
+  created_at: Date;
+  validated_at: Date | null;     
+  status: {
+    draft: boolean;
+    done: boolean;               
+  };
+  products: { id_prod: number; cost: number }[];
+}
