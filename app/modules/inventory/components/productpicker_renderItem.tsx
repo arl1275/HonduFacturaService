764c3543675bd1024ecc5f19107ab1619e318183
arnulfo1275 <arnulfo1275@gmail.com>
2025-12-01 22:11:49 -0600

    product picker page remaster #6

--- app/modules/inventory/components/productpicker_renderItem.tsx
+++ app/modules/inventory/components/productpicker_renderItem.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { View, TextInput, Text } from "react-native";
 import styles from "@/assets/styles/styles";
 import { product } from "@/storage/modals/inventory";
@@ -11,8 +11,13 @@ type props = {
 const ItemRender = ({ val, UpdateProd }: props) => {
   const [cost, setCost] = useState(0);
   const [amount, setAmount] = useState(0);
-
+  const [Local, setLocal] = useState<product>(val);
   const result = cost * amount;
+
+  const OnUpdate = (value : string, field : string) =>{
+    field==="cost" ? handleCostChange(value) : handleAmountChange(value);
+    setLocal((prev)=>({...prev, [field]:parseFloat(value), price : result}));
+  }
 
   const handleCostChange = (e: string) => {
     setCost(parseFloat(e) || 0); 
@@ -21,6 +26,10 @@ const ItemRender = ({ val, UpdateProd }: props) => {
   const handleAmountChange = (e: string) => {
     setAmount(parseFloat(e) || 0);
   };
+
+  useEffect(() => {
+    UpdateProd(Local);    
+  }, [cost, amount]);
 
   return (
     <View style={[styles.flexcomponentsRow, styles.cardborder,
@@ -31,11 +40,11 @@ const ItemRender = ({ val, UpdateProd }: props) => {
       <Text style={[styles.smallText, { color: 'black', flex: 1 }]}>{val.type.consumible ? "CONSUMIBLE" : "STOCK"}</Text>
 
       <View style={[styles.flexcomponentsRow, { justifyContent: 'space-between', width: '30%' }]}>
-        <TextInput onChangeText={handleCostChange}
+        <TextInput onChangeText={(e)=>{OnUpdate(e, "cost") }}
           style={[styles.textinput, { width: '45%', height: 30, flex: 1 }]} 
           placeholder="Cost" keyboardType="numeric" />
 
-        <TextInput onChangeText={handleAmountChange}
+        <TextInput onChangeText={(e)=>{OnUpdate(e, "amountStock")}}
           style={[styles.textinput, { width: '45%', height: 30, flex: 1 }]} 
           placeholder="Amount" keyboardType="numeric" />
 
